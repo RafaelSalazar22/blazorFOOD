222f3500081655e1125ee82cb465b7c7719d1331
RafaelSalazar22 <91817560+RafaelSalazar22@users.noreply.github.com>
2022-03-17 17:40:14 -0600

    Add files via upload

---
+++ Proyecto food/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Food_Blazor
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("google-login")]
+        public async Task<ActionResult> Google()
+        {
+            var properites = new AuthenticationProperties
+            {
+                RedirectUri = "/"
+            };
+            return Challenge(properites, GoogleDefaults.AuthenticationScheme);
+        }
+
+
+    }
+}
---
+++ Proyecto food/LogoutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Food_Blazor
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        public string ReturnUrl { get; private set; }
+
+        public async Task<IActionResult> OnPost(
+            string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            // Clear the existing external cookie
+            await HttpContext
+                   .SignOutAsync();
+            //CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return LocalRedirect("/");
+        }
+    }
+}
